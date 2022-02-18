57434a29d47b411f46f89206d8e5c2b4ca96b409
DorivalRamos <dorivalmillan2@hotmail.com>
2022-02-17 21:56:25 -0300

    Fix: User Create_dto and Update_dto
    adicão do classValidator nos arquivos Create e update DTOs

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,28 @@
+import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
+
+
 export class CreateUserDto {
-    id : string;
-    name : string;
-    email : string;
-    cep: string
+  @IsString()
+  @IsNotEmpty({ message: 'Informe um nome valido' })
+  @MaxLength(200, { message: 'O nome deve ter menos de 200 caracteres' })
+  @MinLength(3, { message: 'O nome deve ter no minimo 3 caracteres' })
+  name : string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Informe um endereço de e-mail' })
+  @IsEmail({}, { message: 'Informe um e-mail válido' })
+  @MaxLength(200, { message: 'O e-mail deve ter menos de 200 caracteres' })
+  email: string;
+    
+  @IsString()
+  @IsNotEmpty({ message: 'Informe um cep valido' })
+  @MaxLength(8, { message: 'O cep deve ter menos de 8 caracteres' })
+  @MinLength(8, { message: 'O cep deve ter no minimo 8 caracteres' })
+  cep: string;
+
+  @IsString()
+  @IsNotEmpty({ message: 'Informe um crm valido' })
+  @MaxLength(8, { message: 'O crm deve ter menos de 8 caracteres' })
+  @MinLength(8, { message: 'O crm deve ter no minimo 8 caracteres' })
+  crm : string
 }
--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,26 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsOptional } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto  {
+    @IsOptional()
+    name: string;
+
+    @IsOptional()
+    email: string;
+
+    @IsOptional()
+    cep: string;
+
+    @IsOptional()
+    crm : string
+
+    @IsOptional()
+    create_at : Date;
+
+    @IsOptional()
+    update_at : Date;
+    
+    @IsOptional()
+    deleted_at : Date;
+}
