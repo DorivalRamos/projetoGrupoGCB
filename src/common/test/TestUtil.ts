e96cad1c84ce2053a6b1399fe1c835d25f70608a
DorivalRamos <dorivalmillan2@hotmail.com>
2022-03-02 17:41:58 -0300

    Fix: TestUltil

--- src/common/test/TestUtil.ts
+++ src/common/test/TestUtil.ts
@@ -1,12 +1,14 @@
 import { User } from "../../users/entities/user.entity";
+import {v4 as uuidv4} from 'uuid';
 
 export default class TestUtil {
     static giveMeAValidUser(): User{
         const user = new User();
-        user.id = '1';
         user.name = 'validName';
         user.email = '  valid@email.com';
         user.cep = 'validCep';
+        user.crm = '123456789';
+        user.id = uuidv4();
 
         return user;
 
