8f5cf11e6448241515be418afd36a637bb5e6df4
DorivalRamos <dorivalmillan2@hotmail.com>
2022-03-07 17:10:11 -0300

    Fix : Refactoring the Specialty Dots

--- src/specialties/dto/create-specialty.dto.ts
+++ src/specialties/dto/create-specialty.dto.ts
@@ -2,7 +2,7 @@ import { IsNotEmpty, IsString, MaxLength, MinLength } from "class-validator";
 
 export class CreateSpecialtyDto {
 
-  @IsString()
+  @IsString({message: 'Informe uma Especialidade valida '})
   @IsNotEmpty({ message: 'Informe um nome valido' })
   @MaxLength(200, { message: 'O nome deve ter menos de 200 caracteres' })
   @MinLength(3, { message: 'O nome deve ter no minimo 3 caracteres' })
--- src/specialties/dto/update-specialty.dto.ts
+++ src/specialties/dto/update-specialty.dto.ts
@@ -1,4 +1,8 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { IsOptional, IsString, MaxLength, MinLength } from 'class-validator';
 import { CreateSpecialtyDto } from './create-specialty.dto';
 
-export class UpdateSpecialtyDto extends PartialType(CreateSpecialtyDto) {}
+export class UpdateSpecialtyDto extends PartialType(CreateSpecialtyDto) {
+    
+    @IsOptional()
+    name : string;}
