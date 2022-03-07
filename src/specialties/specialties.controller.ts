3ba8ba0e5eea2cdf93a991bef3f8ca7be4f8aeaa
DorivalRamos <dorivalmillan2@hotmail.com>
2022-03-07 17:08:38 -0300

    Fix : Refactoring the SpecialtyController
    Refatoriando o Codigo da SpecialtyController

--- src/specialties/specialties.controller.ts
+++ src/specialties/specialties.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ValidationPipe } from '@nestjs/common';
 import { SpecialtiesService } from './specialties.service';
 import { CreateSpecialtyDto } from './dto/create-specialty.dto';
 import { UpdateSpecialtyDto } from './dto/update-specialty.dto';
@@ -7,28 +7,33 @@ import { UpdateSpecialtyDto } from './dto/update-specialty.dto';
 export class SpecialtiesController {
   constructor(private readonly specialtiesService: SpecialtiesService) {}
 
-  @Post()
-  create(@Body() createSpecialtyDto: CreateSpecialtyDto) {
+  @Post('createSpecialties')
+  create(@Body(ValidationPipe) createSpecialtyDto: CreateSpecialtyDto) {
     return this.specialtiesService.create(createSpecialtyDto);
   }
 
-  @Get()
+  @Get('getAllSpecialties')
   findAll() {
     return this.specialtiesService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.specialtiesService.findOne(+id);
+  @Get('findSpecialtiesById/:id')
+  findSpecialtiesById(@Param('id') id: string){
+    return this.specialtiesService.findSpecialtiesById(id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateSpecialtyDto: UpdateSpecialtyDto) {
-    return this.specialtiesService.update(+id, updateSpecialtyDto);
+  @Get('findOneByName/:name')
+  findOneByName(@Param('name') name: string) {
+    return this.specialtiesService.findSpecialtiesByName(name);
   }
 
-  @Delete(':id')
+  @Patch('updateSpecialty/:id')
+  update(@Param('id') id: string, @Body(ValidationPipe) updateSpecialtyDto: UpdateSpecialtyDto) {
+    return this.specialtiesService.update(id, updateSpecialtyDto);
+  }
+
+  @Delete('softDelete/:id')
   remove(@Param('id') id: string) {
-    return this.specialtiesService.remove(+id);
+    return this.specialtiesService.remove(id);
   }
 }
